d1f60f6265020d6b3755a61334ba7a71163bf688
gasricciuti <gas.ricciuti@gmail.com>
2022-03-22 09:55:57 -0300

    Realizado

--- ejercicios_practica/ejercicio_1.py
+++ ejercicios_practica/ejercicio_1.py
@@ -1,7 +1,7 @@
 # Condicionales [Python]
 # Ejercicios de práctica
 
-# Autor: Inove Coding School
+# Autor: Gaston Ricciuti
 # Version: 2.0
 
 # IMPORTANTE: NO borrar los comentarios
@@ -19,12 +19,37 @@ numero_2 = int(input('Ingrese el segundo número:\n'))
 # Compare cual de los dos números es mayor
 # Imprima en pantalla según corresponda
 
+if numero_1 > numero_2:
+    print('{} es mayor a {}'.format(numero_1, numero_2))
+else:
+    print('{} es mayor a {}'.format(numero_2, numero_1))
+    
 # Verifique si el numero_1 positivo, negativo o cero
 # Imprima el resultado en cada caso
+
+if numero_1 > 0:
+    print('{} es un numero positivo'.format(numero_1))
+elif numero_1 < 0:
+    print('{} es un numero negativo'.format(numero_1))
+else:
+    print('el numero es 0')
+
+
 
 # Verifique si el numero_1 es mayor a 0 y menor a 100
 # Imprima en pantalla si se cumple o no la condición
 
+if (numero_1 > 0) and (numero_1 < 100):
+    print('el primer numero esta entre 0 y 100')
+else:
+    print('el primer numero es negativo, cero o mayor a 100')
+    
 # Verifique si el numero_1 es menor a 10 o el numero_2
 # es mayor a -2
 # Imprima en pantalla si se cumple o no la condición
+
+if (numero_1 < 10) or (numero_2 > -2):
+    print('{} es mayor a 10 o {} es mayor a -2'.format(numero_1, numero_2))
+else:
+    print('No cumple la condicion')
+    
