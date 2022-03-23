74f55eb5368529609113401a55f9ffbd9338242c
gasricciuti <gas.ricciuti@gmail.com>
2022-03-23 13:38:12 -0300

    Realizado

--- ejercicios_profunidzacion/profundizacion_1.py
+++ ejercicios_profunidzacion/profundizacion_1.py
@@ -23,3 +23,29 @@ si el resultado es positivo, negativo o cero.
 print('Ejercicios de práctica con números')
 # Empezar aquí la resolución del ejercicio
 
+numero_1 = int(input('Ingrese el primer número:\n'))
+
+numero_2 = int(input('Ingrese el segundo número:\n'))
+
+
+
+if numero_1 > numero_2:
+    if numero_2 > 0:
+        resultado = numero_1 - numero_2
+    else:
+        resultado = numero_1 + numero_2                    
+elif numero_2 > numero_1:
+    if numero_1 > 0:
+        resultado = numero_2 - numero_1
+    else:            
+        resultado = numero_2 + numero_1   
+else:
+    resultado = numero_1 - numero_2
+
+if resultado > 0:
+    print("El resultado es Positivo", resultado)
+elif resultado < 0:
+    print("El resultado es Negativo", resultado)    
+else:
+    print("El resultado es Cero")
+
